9155463d902d8e26ed79629872e2f8abea53df36
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-04-04 11:42:41 +0300

    Finished on Resposiveness

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-
-    //Navigation
+    
     document.querySelectorAll(".nav-link").forEach(link => {
         link.addEventListener("click", function () {
             document.querySelector(".nav-link.active")?.classList.remove("active");
             this.classList.add("active");
         });
     });
-    
-})
+});
