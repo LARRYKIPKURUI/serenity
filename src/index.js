ea569d83298b1ca5c047bc06d22feea1693f9206
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-04-04 11:11:38 +0300

    Making the site REsponsive while tweaking the UI

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //Navigation
+    document.querySelectorAll(".nav-link").forEach(link => {
+        link.addEventListener("click", function () {
+            document.querySelector(".nav-link.active")?.classList.remove("active");
+            this.classList.add("active");
+        });
+    });
+    
+})
